4c969824d89d96dc9f155cb6aa6d86bc97a5cb1a
isaac Mathias <xclusivzik@hotmail.com>
2018-08-24 00:01:43 +0100

    greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 'use strict'
 const path =require('path');
+const http = require('http');
 const express= require('express');
+const socketIO = require('socket.io');
 
 
 
@@ -10,7 +12,55 @@ const port = process.env.PORT || 3000;
 //console.log(publicPath);
 
 var app =express ();
+var server = http.createServer(app);
+var io =socketIO(server);
+
+
+
+
 app.use(express.static(publicPath));
-app.listen(port, () =>{
+
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+
+	//for greeting the user
+	socket.emit('newMessage',{
+		from:'Admin',
+		text: 'Welcome to the chat App',
+		createAt: new Date(). getTime()  
+	});
+
+	socket.broadcast.emit('newMessage',{
+		from: 'Admin',
+		text: 'New user joined',
+		createAt: new Date(). getTime()  
+	});
+
+	
+socket.on('createMessage',(message) =>{
+	console.log('createMessage', message);
+	io.emit('newMessage', {
+		from: message.from,
+		text: message.text,
+		createAt: new Date(). getTime()  
+
+	});
+	// socket.broadcast.emit('newMessage',{
+
+	// 	from: message.from,
+	// 	text: message.text,
+	// 	createAt: new Date(). getTime()  
+
+	// });
+
+
+});
+	socket.on('disconnet', () => {
+		console.log('User disconnected');
+	});
+});
+
+server.listen(port, () =>{
 	console.log('server is up on ${port}');
 });
