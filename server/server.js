99250b1393997aaa53c3611500f13d29ea294a7e
isaac Mathias <xclusivzik@hotmail.com>
2018-08-25 02:22:20 +0100

    final projects code

--- server/server.js
+++ server/server.js
@@ -1,61 +1,67 @@
-
-const path =require('path');
+const path = require('path');
 const http = require('http');
-const express= require('express');
+const express = require('express');
 const socketIO = require('socket.io');
+
 const {generateMessage, generateLocationMessage} = require('./utils/message');
-
-
-
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-//console.log(__dirname + '/../public');
-//console.log(publicPath);
-
-var app =express ();
+var app = express();
 var server = http.createServer(app);
-var io =socketIO(server);
-
-
-
+var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-	console.log('New user connected');
+  console.log('New user connected');
 
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback('Name and room name are required.');
+    }
 
-	//for greeting the user from admin
-	socket.emit('newMessage', generateMessage('Admin', 'Welcome to ChatApp'));
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
 
-	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    callback();
+  });
 
-	
-socket.on('createMessage',(message, callback) =>{
-	console.log('createMessage', message);
-	//mesage from user to user
-	io.emit('newMessage', generateMessage(message.from, message.text));
-	callback();
-	// socket.broadcast.emit('newMessage',{
+  socket.on('createMessage', (message, callback) => {
+    var user = users.getUser(socket.id);
 
-	// 	from: message.from,
-	// 	text: message.text,
-	// 	createAt: new Date(). getTime()  
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
 
-	// });
+    callback();
+  });
 
+  socket.on('createLocationMessage', (coords) => {
+    var user = users.getUser(socket.id);
 
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));  
+    }
+  });
+
+  socket.on('disconnect', () => {
+    var user = users.removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+    }
+  });
 });
-socket.on('createLoctionMessage',(coords) =>{
-	io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
-});
 
-	socket.on('disconnet', () => {
-		console.log('User disconnected');
-	});
-});
-
-server.listen(port, () =>{
-	console.log('server is up on ${port}');
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,16 +1,19 @@
+var moment = require('moment');
 
-var generateMessage = (from,text) =>{
-	return{
-		from,
-		text,
-		createAt: new Date(). getTime()  
-	};
+var generateMessage = (from, text) => {
+  return {
+    from,
+    text,
+    createdAt: moment().valueOf()
+  };
 };
-var generateLocationMessage = (from, latitude, longitude) =>{
-	return{
-		from,
-		url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-		createAt: new Date(). getTime()  
-	};
+
+var generateLocationMessage = (from, latitude, longitude) => {
+  return {
+    from,
+    url: `https://www.google.com/maps?q=${latitude},${longitude}`,
+    createdAt: moment().valueOf()
+  };
 };
+
 module.exports = {generateMessage, generateLocationMessage};
